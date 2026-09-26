eed41dbc0a2be73de2590a8781756be13bce6eda
prajwal-patil-8550 <prajwalpatil8550@gmail.com>
2026-09-26 14:05:51 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        vector<int>ans(n, 0);
+        stack<int>st;
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
+                st.pop();
+            }
+            if(!st.empty()){
+                //temperatures[i] < temperatures[st.top()]
+                ans[i]=st.top()-i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
